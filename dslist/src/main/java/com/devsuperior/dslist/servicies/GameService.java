b80e4c2cac7d166fc8a73d17b6049127c6b3fbf8
Meletinho <vinipitta01@gmail.com>
2025-08-24 02:10:21 -0300

    Database seed update

--- dslist/src/main/java/com/devsuperior/dslist/servicies/GameService.java
+++ dslist/src/main/java/com/devsuperior/dslist/servicies/GameService.java
@@ -1,30 +1,40 @@
-package com.devsuperior.dslist.seervicies;
+package com.devsuperior.dslist.servicies;
 
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service
 //Chama o repository no BD, devolvendo o DTO
+@Service
 public class GameService {
 
 
     @Autowired
     private GameRepository gameRepository;
 
+    //Assegura que não irei bloquear meu banco p escrita
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
 
     public List<GameMinDTO> findAll() {
            //Faz uma busca no banco de dados pra listar todos os jogos no BD
-           List<Game> result =  gameRepository.findAll();
-           List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(GameMinDTO::new).collect(Collectors.toList());
 
-           return dto;
+
     }
 
 
