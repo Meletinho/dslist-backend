4201ae2b66600d5028583c9f20e4e7ac374fbb7a
Meletinho <vinipitta01@gmail.com>
2025-08-24 23:53:49 -0300

    Move games on list

--- dslist/src/main/java/com/devsuperior/dslist/servicies/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/servicies/GameListService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,18 +21,38 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
-    //Assegura que não irei bloquear meu banco p escrita
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll() {
-           //Faz uma busca no banco de dados pra listar todos os jogos no BD
         List<GameList> result = gameListRepository.findAll();
-        return result.stream().map(GameListDTO::new).collect(Collectors.toList());
-
+        return result.stream().map(GameListDTO::new).toList();
     }
 
+    @Transactional
     public void move(Long listId, int sourceIndex, int destinationIndex) {
-        List<GameMinProjection> list = gameListRepository.searchByList(listId);
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++) {
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
+
+    @Transactional(readOnly = true)
+    public GameListDTO findById(Long id) {
+        GameList entity = gameListRepository.findById(id).get();
+        return new GameListDTO(entity);
+    }
+
+
 
 
 }
